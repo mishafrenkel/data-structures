c02f7199d0a78c49954f6a956454b9596b5c9b48
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-20 15:41:20 -0800

    Implemented solutions for queue and stack instantiations using functional style

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,55 @@
 var Queue = function() {
   var someInstance = {};
 
+  // create a global count that stores the property name as a number
+  someInstance.count = 0; 
+  // Declare a global count to keep track of size
+  someInstance.lowestCount = 0;
+   
   // Use an object with numeric keys to store values
   var storage = {};
+  // assign storage as a property of someInstance
+  someInstance.storage = storage; 
+
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add value to the queue 
+    // increment count  
+    if (value) {
+      someInstance.storage[someInstance.count] = value;
+      // increment the starting index by one
+      someInstance.count++;
+      return someInstance.storage[someInstance.count];
+    }
   };
 
   someInstance.dequeue = function() {
+    // save node to delete in var  
+    // Delete node 
+    // Increment lowest count 
+    // Return saved node
+    if (someInstance.count - 0 === 0) {
+      return undefined;
+    } 
+
+    var result = someInstance.storage[someInstance.lowestCount];
+    delete someInstance.storage[someInstance.lowestCount];
+    someInstance.lowestCount++;
+    return result; 
   };
 
   someInstance.size = function() {
+    if (!someInstance.size) {
+      someInstance.size = 0;
+    } else {
+      someInstance.size = someInstance.count - someInstance.lowestCount;
+      if (someInstance.size < 0) {
+        someInstance.size = 0;
+      }
+    } 
+    return someInstance.size;
   };
-
   return someInstance;
 };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,39 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // Assign storage to someInstance  
+  someInstance.storage = storage;
+  someInstance.count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance.storage[someInstance.count] = value;
+    someInstance.count++;
   };
 
   someInstance.pop = function() {
+    // Check to see if the stack is empty 
+    if (someInstance.count === 0) {
+      return undefined; 
+    } else {
+      someInstance.count--;
+      var result = someInstance.storage[someInstance.count];  //#endregion
+      delete someInstance.storage[someInstance.count];
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return someInstance.count;
   };
+
+  
 
   return someInstance;
 };
+
+// this.count--;
+// var result = this.storage[this.count];
+// delete this.storage[this.count];
+// return result;
+// }

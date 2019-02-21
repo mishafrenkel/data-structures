206119bebf4f49d6ef268db5a714cf813f899d95
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-21 10:48:12 -0800

    Implemented E6 style queue data structure using a psuedoclassical pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,40 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    // initialize items in queue 
+    this.storage = {};
+    this.count = 0; 
+    this.lowestCount = 0;
   }
 
+  enqueue(value) {
+    // add value to storage at key this.count 
+    if (value) {
+      this.storage[this.count] = value;
+      this.count++;
+      return this;
+    } 
+  }
+  
+  dequeue() {
+    //pull out the first item from the queue
+    if (this.count > 0) {
+      var result = this.storage[this.lowestCount]; 
+      delete this.storage[this.lowestCount];
+      this.lowestCount++;
+      return result;
+    }
+  }
+  size() {
+    //get the length of queue
+    if (!this.size) {
+      this.size = 0;
+    } else {
+      this.size = this.count - this.lowestCount;
+      if (this.size < 0) {
+        this.size = 0;
+      }
+    }
+    return this.size;
+  }
 }
+

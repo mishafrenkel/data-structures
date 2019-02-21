5f92006ba00922ee94a77f56b7581191ce0cdcb0
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-21 10:32:44 -0800

    implemented  stack data structure using ES6 psuedoclassical pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.index = 0;
+  }
+  size() {
+    return this.index;
+  }
+  push(value) {
+    this.storage[this.index] = value;
+    this.index++;
+  }
+  pop() {
+    if (this.index > 0) {
+      this.index--;
+      var result = this.storage[this.index];
+      delete this.storage[this.index]; 
+      return result;
+    } else {
+      return undefined;
+    }
   }
 
 }
+
+

e46f3eda142b1578e50eb5aaae07aa86ef3337b8
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-22 11:02:00 -0800

    Implement singley linked list type of data structure
    Constraint: constant time complexity for insertion, deletion with an average run-time of O(n).

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,42 @@
+// head     tail
+//  ↓         ↓
+// 12 → 99 → 37 → ∅
+// const list = {
+//     head: → { value: 12, next: ⤵ }
+//             { value: 99, next: ⤵ }
+//     tail: → { value: 37, next: ∅ }
+// };
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = oldHead.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } 
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
@@ -24,6 +51,12 @@ var Node = function(value) {
   return node;
 };
 
+
+
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? //#endregion
+ Constant time is maintained for adding, removal. Linked list will have  
+ an average time complexity of O(n) with a an O(1) for insertion and deletion.
  */
+
+

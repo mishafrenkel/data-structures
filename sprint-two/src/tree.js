4734270cce984b8c4c9ee8f626f11abc15cb00a7
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-22 15:35:43 -0800

    Implement tree.js
    Implementation of a tree node structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  Object.assign(newTree, treeMethods);
+
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,13 +13,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(targetValue) {
+  if (this.value === targetValue) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(targetValue)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
  */

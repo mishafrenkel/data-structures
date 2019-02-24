535418784bc7900bbe63a1208081c17ecd50711e
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-23 16:18:36 -0800

    implement set data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,13 @@
-var BinarySearchTree = function(value) {
-};
+// // Left and right property nodes 
+
+// var BinarySearchTree = function(value) {
+// };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+var BinarySearchTree = function(value) {
+  
+};
+  
